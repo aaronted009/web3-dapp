611c40767230de85b11ddb92d8262a06b62a8c89
Aaron <aaronpromail@gmail.com>
2024-04-21 12:44:11 +0100

    Adding templates

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def home(request: Request) -> Response:
+    """Home page."""
+    return templates.TemplateResponse("home.jinja", {"request": request})
+
+
+@app.get("/ticket/")
+async def buy_ticket(request: Request) -> Response:
+    """Buy ticket page."""
+    return templates.TemplateResponse("buy-ticket.jinja", {"request": request})
